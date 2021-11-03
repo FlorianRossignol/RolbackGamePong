0ee85f531b8903a7dfd57a866f6039e020e45922
Florian Rossignol <rossignol.florian.fr@gmail.com>
2021-11-03 16:08:59 +0100

    finished game condition

--- game/src/game/ball_manager.cpp
+++ game/src/game/ball_manager.cpp
@@ -21,34 +21,38 @@ namespace game
                 
                auto ball = physicsManager_.GetBody(entity);
                 
-               auto player = playerCharacterManager_.GetComponent(entity);
+               
                 
-               auto playerone = gameManager_.GetEntityFromPlayerNumber(0);
-               auto playertwo = gameManager_.GetEntityFromPlayerNumber(1);
-               core::Vec2f vit = {1,-1};
+               
+               
+               //core::Vec2f vit = {1,-1};
                //if the player lost ball in right the first player lose hp 
                 if (ball.position.x > rectShapeDim.x / 100)
                 {
-                    
+                    auto firstPlayerEntity = gameManager_.GetEntityFromPlayerNumber(0);
                     ball.position = core::Vec2f{0,0};
-                    vit = { 0, 0 };
-                    
+                    //vit = { 0, 0 };
+                    auto player = playerCharacterManager_.GetComponent(firstPlayerEntity);
                     player.health--;
+                    playerCharacterManager_.SetComponent(firstPlayerEntity,player);
                 }
                 if (ball.position.x < -rectShapeDim.x / 100)
                 {
                    
                     ball.position = core::Vec2f{ 0,0 };
-                    vit = { 0, 0 };
+                    //vit = { 0, 0 };
+                    auto secondPlayerEntity = gameManager_.GetEntityFromPlayerNumber(1);
+                    auto player = playerCharacterManager_.GetComponent(secondPlayerEntity);
                     
                     player.health--;
+                    playerCharacterManager_.SetComponent(secondPlayerEntity, player);
                 }
                 
                 if (ball.position.y > rectShapeDim.y / 100 ||
                     ball.position.y < -rectShapeDim.y /100)
                 {
                     
-                    ball.velocity = core::Vec2f{ ball.velocity.x,-ball.velocity.y } + vit ;
+                    ball.velocity = core::Vec2f{ ball.velocity.x,-ball.velocity.y }; //+ vit ;
                     
                 }
                 physicsManager_.SetBody(entity, ball);
